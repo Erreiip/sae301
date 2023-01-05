de49b94d8f18bee84b31fd4f4ecaa1d43a44aef5
nz76 <kzyord@gmail.com>
2023-01-05 11:02:26 +0100

    Update PanelCartesInteraction.java

--- client/src/vue/panels/PanelCartesInteraction.java
+++ client/src/vue/panels/PanelCartesInteraction.java
@@ -6,6 +6,8 @@ import client.src.metier.common.Wagon;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -43,12 +45,28 @@ public class PanelCartesInteraction extends JPanel
         this.alWagons.add(new Wagon(2, "wagon3"));
 
         String[] columsName = {"Carte","Nombre"};
-        String[][] data = new String[this.alWagons.size()][2];
+        Object[][] data = new Object[this.alWagons.size()][2];
         for (int i = 0; i < this.alWagons.size(); i++)
         {
             data[i][0] = this.alWagons.get(i).getFileRecto();
-            
+            data[i][1] = "sheesh";
         }
+
+        DefaultTableModel model = new DefaultTableModel(data, columsName);
+
+        JTable table = new JTable(model) 
+        {
+            public Class getColumnClass(int column) 
+            {
+                return (column == 0) ? Icon.class : Object.class;
+            }
+        };
+
+        table.setPreferredSize(new Dimension(280,550));
+        table.setRowHeight(85);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        this.panelWagons.add(scrollPane);
 
 
 
