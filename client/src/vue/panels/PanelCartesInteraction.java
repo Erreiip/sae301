d07bcdbccbd8c4b0817e8740908dc92d9b64e8b2
nz76 <kzyord@gmail.com>
2023-01-05 10:04:44 +0100

    Update PanelCartesInteraction.java

--- client/src/vue/panels/PanelCartesInteraction.java
+++ client/src/vue/panels/PanelCartesInteraction.java
@@ -8,10 +8,15 @@ import javax.swing.*;
 import java.awt.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PanelCartesInteraction extends JPanel
 {
     private Controleur ctrl;
+
+    //private final Map<String, ImageIcon> imageMap;
 
     private JTabbedPane tpCartes;
         private JPanel panelWagons;
@@ -31,22 +36,47 @@ public class PanelCartesInteraction extends JPanel
         this.panelObjectifs = new JPanel();
 
         this.alWagons = this.ctrl.getMainJoueur();
+
         this.alWagons.add(new Wagon(1, "wagon1"));
         this.alWagons.add(new Wagon(2, "wagon2"));
         this.alWagons.add(new Wagon(3, "wagon3"));
+
         String[] strWagons = new String[this.alWagons.size()];
         for (Wagon w : this.alWagons)
         {
             strWagons[this.alWagons.indexOf(w)] = w.toString();
         }
-        for (int i = 0; i < strWagons.length; i++)
-            System.out.println(strWagons[i] + "oui");
+        
+        //imageMap = WagonListRenderer.createImageMap(this.alWagons);
+        JList<String> listWagons = new JList<String>(strWagons);
+        listWagons.setCellRenderer(new WagonListRenderer());
 
-
+        this.panelWagons.add(listWagons);
 
         this.tpCartes.addTab("Wagons", this.panelWagons);
         this.tpCartes.addTab("Objectifs", this.panelObjectifs);
 
         this.add(this.tpCartes);
+    }
+
+    public class WagonListRenderer extends DefaultListCellRenderer
+    {
+        @Override
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
+        {
+            JLabel label = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            label.setIcon(new ImageIcon("client/src/vue/images/wagon1.png"));
+            return label;
+        }
+
+        private Map<String, ImageIcon> createImageMap(ArrayList<Wagon> list)
+        {
+            Map<String, ImageIcon> map = new HashMap<>();
+            for (Wagon w : list)
+            {
+                map.put(String.valueOf(w.couleur), new ImageIcon(w.getFileRecto()));
+            }
+            return map;
+        }
     }
 }
