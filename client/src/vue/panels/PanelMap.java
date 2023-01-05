f88601ee25ed6da7cd55059e84e69830bad99eac
nz76 <kzyord@gmail.com>
2023-01-05 17:51:25 +0100

    Update PanelMap

--- client/src/vue/panels/PanelMap.java
+++ client/src/vue/panels/PanelMap.java
@@ -1,11 +1,17 @@
 package client.src.vue.panels;
 
 import client.src.Controleur;
+import client.src.metier.common.RectangleNom;
+import client.src.metier.common.Route;
+import client.src.metier.common.Ville;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.geom.AffineTransform;
 
 public class PanelMap extends JPanel
 {
@@ -30,9 +36,97 @@ public class PanelMap extends JPanel
     {
         super.paintComponent(g);
         
-        Graphics g2d = (Graphics) g;
+        Graphics2D g2d = (Graphics2D) g;
 
         g.drawImage(this.fond, this.x, this.y, this.fond.getWidth(), this.fond.getHeight(), null);
+
+        for (Ville ellipse : this.ctrl.getAlVilles())
+        {
+            for (Route r : this.ctrl.getAlRoutes())
+            {
+                g.setColor(r.getCouleur1());
+                
+                int tailleX = Math.subtractExact((int) (r.getVille1().getCenterX() + r.getVille1().getTaille()), (int) (r.getVille2().getCenterX() + r.getVille1().getTaille()));
+                int tailleY = Math.subtractExact((int) (r.getVille1().getCenterY() + r.getVille1().getTaille()), (int) (r.getVille2().getCenterY() + r.getVille1().getTaille()));
+
+                int width = tailleX / (r.getCout()+1);
+                int height = tailleY / (r.getCout()+1);
+
+                int longueur = Math.max( Math.abs(tailleX / r.getCout()), Math.abs(tailleY/r.getCout()));
+                if ( r.getCout() > 1 ) longueur -= longueur /r.getCout();
+                int largeur =  18;
+
+                Point n1,n3;
+
+                double angle = Math.atan((double) (r.getVille2().getY() - r.getVille1().getY()) / 
+                                              (r.getVille2().getX() - r.getVille1().getX())  );
+
+                int adj = (int) (12 * Math.cos(angle + 1.57)); //90° = 1.57
+                int opp = (int) (12 * Math.sin(angle + 1.57));
+
+                n1 = new Point((int)r.getVille1().getX() + adj, (int)r.getVille1().getY() + opp );
+
+                n3 = new Point((int)r.getVille1().getX() - adj, (int)r.getVille1().getY() - opp );
+
+                Ville v = r.getVille1();
+
+                int x = width;
+                int y = height;               
+                
+
+                RoundRectangle2D fig1, fig2 = null;
+                Shape fig3;
+                for ( int cpt = 0; cpt < r.getCout(); cpt++)
+                {
+                    if ( r.estDouble() )
+                    {   
+                        fig1 = new RoundRectangle2D.Double( n1.getX() - x , n1.getY() - y , longueur, largeur, 15d, 15d);
+                        fig2 = new RoundRectangle2D.Double( n3.getX() - x , n3.getY() - y , longueur, largeur, 15d, 15d); //v.getCenterX() - x - longueur/2 , v.getCenterY() - y - largeur/2 , longueur, largeur, 15d, 15d
+
+                    } else {
+                        fig1 = new RoundRectangle2D.Double( v.getCenterX() - x - longueur/2 , v.getCenterY() - y - largeur/2, longueur, largeur, 15d, 15d);
+                    }
+
+                    AffineTransform t = new AffineTransform();
+                    t.rotate(angle, fig1.getCenterX(), fig1.getCenterY());
+                    fig3 = t.createTransformedShape(fig1);
+
+                    g2d.setColor(r.getCouleur1());
+                    g2d.fill(fig3);
+
+                    g2d.setColor(Color.BLACK);
+                    g2d.draw(fig3);
+
+                    if ( r.estDouble() )
+                    {
+                        AffineTransform t2 = new AffineTransform();
+                        t2.rotate(angle, fig2.getCenterX(), fig2.getCenterY());
+                        fig3 = t2.createTransformedShape(fig2);
+                        
+                        g2d.setColor(r.getCouleur2());
+                        g2d.fill(fig3);
+                        
+                        g2d.setColor(Color.BLACK);
+                        g2d.draw(fig3);
+                    }
+
+                    x += width;
+                    y += height;
+                }
+            }
+        }
+
+        // ELLIPSE VILLE
+        for (Ville v : this.ctrl.getAlVilles())
+        {
+            g2d.setColor(v.getCouleur());
+            g2d.fillOval((int)v.getX(), (int)v.getY(), 20, 20);
+            g2d.setColor(Color.BLACK);
+            g2d.drawOval((int)v.getX(), (int)v.getY(), 20, 20);
+        }
+
+        // RECTANGLE NOM
+
     }
 
     public Dimension getPreferredSize() 
