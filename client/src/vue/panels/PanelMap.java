c765e32c6d3126fe3a83d6ce3b0d4ce5286ba162
Erreiip <lolpierre154@gmail.com>
2023-01-06 07:51:17 +0100

    add RectangleNom on the map

--- client/src/vue/panels/PanelMap.java
+++ client/src/vue/panels/PanelMap.java
@@ -40,79 +40,76 @@ public class PanelMap extends JPanel
 
         g.drawImage(this.fond, this.x, this.y, this.fond.getWidth(), this.fond.getHeight(), null);
 
-        for (Ville ellipse : this.ctrl.getAlVilles())
+        for (Route r : this.ctrl.getAlRoutes())
         {
-            for (Route r : this.ctrl.getAlRoutes())
+            g.setColor(r.getCouleur1());
+            
+            int tailleX = Math.subtractExact((int) (r.getVille1().getCenterX() + r.getVille1().getTaille()), (int) (r.getVille2().getCenterX() + r.getVille1().getTaille()));
+            int tailleY = Math.subtractExact((int) (r.getVille1().getCenterY() + r.getVille1().getTaille()), (int) (r.getVille2().getCenterY() + r.getVille1().getTaille()));
+
+            int width = tailleX / (r.getCout()+1);
+            int height = tailleY / (r.getCout()+1);
+
+            int longueur = Math.max( Math.abs(tailleX / r.getCout()), Math.abs(tailleY/r.getCout()));
+            if ( r.getCout() > 1 ) longueur -= longueur /r.getCout();
+            int largeur =  18;
+
+            Point n1,n3;
+
+            double angle = Math.atan((double) (r.getVille2().getY() - r.getVille1().getY()) / 
+                                            (r.getVille2().getX() - r.getVille1().getX())  );
+
+            int adj = (int) (12 * Math.cos(angle + 1.57)); //90° = 1.57
+            int opp = (int) (12 * Math.sin(angle + 1.57));
+
+            n1 = new Point((int)r.getVille1().getX() + adj, (int)r.getVille1().getY() + opp );
+
+            n3 = new Point((int)r.getVille1().getX() - adj, (int)r.getVille1().getY() - opp );
+
+            Ville v = r.getVille1();
+
+            int x = width;
+            int y = height;               
+            
+
+            RoundRectangle2D fig1, fig2 = null;
+            Shape fig3;
+            for ( int cpt = 0; cpt < r.getCout(); cpt++)
             {
-                g.setColor(r.getCouleur1());
-                
-                int tailleX = Math.subtractExact((int) (r.getVille1().getCenterX() + r.getVille1().getTaille()), (int) (r.getVille2().getCenterX() + r.getVille1().getTaille()));
-                int tailleY = Math.subtractExact((int) (r.getVille1().getCenterY() + r.getVille1().getTaille()), (int) (r.getVille2().getCenterY() + r.getVille1().getTaille()));
+                if ( r.estDouble() )
+                {   
+                    fig1 = new RoundRectangle2D.Double( n1.getX() - x , n1.getY() - y , longueur, largeur, 15d, 15d);
+                    fig2 = new RoundRectangle2D.Double( n3.getX() - x , n3.getY() - y , longueur, largeur, 15d, 15d); //v.getCenterX() - x - longueur/2 , v.getCenterY() - y - largeur/2 , longueur, largeur, 15d, 15d
 
-                int width = tailleX / (r.getCout()+1);
-                int height = tailleY / (r.getCout()+1);
+                } else {
+                    fig1 = new RoundRectangle2D.Double( v.getCenterX() - x - longueur/2 , v.getCenterY() - y - largeur/2, longueur, largeur, 15d, 15d);
+                }
 
-                int longueur = Math.max( Math.abs(tailleX / r.getCout()), Math.abs(tailleY/r.getCout()));
-                if ( r.getCout() > 1 ) longueur -= longueur /r.getCout();
-                int largeur =  18;
+                AffineTransform t = new AffineTransform();
+                t.rotate(angle, fig1.getCenterX(), fig1.getCenterY());
+                fig3 = t.createTransformedShape(fig1);
 
-                Point n1,n3;
+                g2d.setColor(r.getCouleur1());
+                g2d.fill(fig3);
 
-                double angle = Math.atan((double) (r.getVille2().getY() - r.getVille1().getY()) / 
-                                              (r.getVille2().getX() - r.getVille1().getX())  );
+                g2d.setColor(Color.BLACK);
+                g2d.draw(fig3);
 
-                int adj = (int) (12 * Math.cos(angle + 1.57)); //90° = 1.57
-                int opp = (int) (12 * Math.sin(angle + 1.57));
-
-                n1 = new Point((int)r.getVille1().getX() + adj, (int)r.getVille1().getY() + opp );
-
-                n3 = new Point((int)r.getVille1().getX() - adj, (int)r.getVille1().getY() - opp );
-
-                Ville v = r.getVille1();
-
-                int x = width;
-                int y = height;               
-                
-
-                RoundRectangle2D fig1, fig2 = null;
-                Shape fig3;
-                for ( int cpt = 0; cpt < r.getCout(); cpt++)
+                if ( r.estDouble() )
                 {
-                    if ( r.estDouble() )
-                    {   
-                        fig1 = new RoundRectangle2D.Double( n1.getX() - x , n1.getY() - y , longueur, largeur, 15d, 15d);
-                        fig2 = new RoundRectangle2D.Double( n3.getX() - x , n3.getY() - y , longueur, largeur, 15d, 15d); //v.getCenterX() - x - longueur/2 , v.getCenterY() - y - largeur/2 , longueur, largeur, 15d, 15d
-
-                    } else {
-                        fig1 = new RoundRectangle2D.Double( v.getCenterX() - x - longueur/2 , v.getCenterY() - y - largeur/2, longueur, largeur, 15d, 15d);
-                    }
-
-                    AffineTransform t = new AffineTransform();
-                    t.rotate(angle, fig1.getCenterX(), fig1.getCenterY());
-                    fig3 = t.createTransformedShape(fig1);
-
-                    g2d.setColor(r.getCouleur1());
+                    AffineTransform t2 = new AffineTransform();
+                    t2.rotate(angle, fig2.getCenterX(), fig2.getCenterY());
+                    fig3 = t2.createTransformedShape(fig2);
+                    
+                    g2d.setColor(r.getCouleur2());
                     g2d.fill(fig3);
-
+                    
                     g2d.setColor(Color.BLACK);
                     g2d.draw(fig3);
-
-                    if ( r.estDouble() )
-                    {
-                        AffineTransform t2 = new AffineTransform();
-                        t2.rotate(angle, fig2.getCenterX(), fig2.getCenterY());
-                        fig3 = t2.createTransformedShape(fig2);
-                        
-                        g2d.setColor(r.getCouleur2());
-                        g2d.fill(fig3);
-                        
-                        g2d.setColor(Color.BLACK);
-                        g2d.draw(fig3);
-                    }
-
-                    x += width;
-                    y += height;
                 }
+
+                x += width;
+                y += height;
             }
         }
 
@@ -127,6 +124,21 @@ public class PanelMap extends JPanel
 
         // RECTANGLE NOM
 
+        for ( Ville v : this.ctrl.getAlVilles())
+        {
+            RectangleNom r = v.getRectangle();
+            
+            r.maj();
+
+            g.setColor(Color.WHITE);
+            g2d.fill(r);
+
+            g.setColor(Color.BLACK);
+            g2d.draw(r);
+
+            g.setColor(Color.BLACK);
+            g2d.drawString(r.getNom(), r.getPosX()+2, (int) r.getCenterY() + 5);
+        }
     }
 
     public Dimension getPreferredSize() 
