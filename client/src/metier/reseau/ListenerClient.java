a3c2a8890413beac26e183f32c047acb75ed494c
Erreiip <lolpierre154@gmail.com>
2023-01-06 17:00:37 +0100

    Change Server and client

--- client/src/metier/reseau/ListenerClient.java
+++ client/src/metier/reseau/ListenerClient.java
@@ -26,7 +26,6 @@ public class ListenerClient extends Listener
             this.client.setAction(jeu);
         }
 
-        /*
         if ( object instanceof String)
         {
             String s = (String) object;
@@ -37,7 +36,6 @@ public class ListenerClient extends Listener
                 new ClientFile(tabString[1]);
             }
         }
-        */
 
         if (object instanceof Joueur)
         {
